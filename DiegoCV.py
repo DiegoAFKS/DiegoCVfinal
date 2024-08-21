8decad04ca9d46f969855ddeceb35ff20da44f31
DiegoAFKS <legendariesproductions@gmail.com>
2024-08-20 18:22:01 -0600

    Add files via upload

---
+++ DiegoCV.py
@@ -0,0 +1,69 @@
+
+import streamlit as st
+st.markdown("# DIEGO ESPINOZA CISNEROS")
+st.markdown("### Administración y Finanzas")
+st.markdown("------------")
+
+st.markdown(
+    """
+    <style>
+    /* Color del fondo */
+    .stApp {
+        background-color: #001f3f;  
+    }
+   
+    </style>
+
+    
+    """,
+    unsafe_allow_html=True
+)
+
+col1, col2 = st.columns([1,1.5])
+#COLUMNA 1
+with col1:
+    st.image("C:/Users/deces/Documents/UP/Programacion/WhatsApp Image 2024-08-18 at 19.18.55.jpeg",width=220)
+
+    st.markdown("#### **EDUCACION**") 
+    st.markdown("###### Universidad Panamericana Campues Guadalajara Lic. Administración y Finanzas EN CURSO ")
+    st.markdown(" ")
+    st.markdown("#### HABILIDADES") 
+    st.markdown(" ")
+    st.markdown("###### Manejo de Paquetería Office *Excel Avanzado")
+    st.markdown("###### Trabajo en equipo") 
+    st.markdown("###### Aprendizaje rápido")
+    st.markdown("###### Pensamiento crítico")
+    st.markdown("###### Capacidad de trabajar con honorarios flexibles")
+    st.markdown("###### Habilidad analítica")
+    st.markdown(" ")
+    
+    st.markdown("#### IDIOMAS") 
+    st.markdown("###### Español Nativo")
+    st.markdown("###### Inglés Avanzado")
+
+with col2:
+    st.markdown("#### CONTACTO")
+    st.markdown("###### +52 3318604072")
+    st.markdown("###### dec.espinoza29@gmail.com")
+    st.markdown("###### Guadalajara, Jalisco")
+    st.markdown(" ")
+    st.markdown(" ")
+    
+    st.markdown("#### ACERCA DE MI")
+    st.markdown("###### Actualmente curso el séptimo semestre de la carrera de Administración y Finanzas en la Universidad Panamericana, Campus Guadalajara. Soy una persona con facilidad de aprendizaje y adaptarme de manera rápida. Me gusta ser responsable y centrado en mis actividades diarias, lo que me permite organizar mis tiempos para cumplir mis objetivos.")
+    st.markdown(" ")
+    st.markdown(" ")
+    st.markdown("#### EXPERIENCIA")
+    st.markdown(" ")
+    st.markdown("###### Apoyo administrativo INEEJAD Instituto Estatal para la Educación de Jóvenes y Adultos (Enero a Junio 2023)")
+    st.markdown("###### Apoyo administrativo Boutique Manta del Lago (Enero a Diciembre 2020")
+    st.markdown(" ")
+    st.markdown("#### EXPERIENCIA EXTRACURRICULAR")
+    st.markdown("###### Miembro y capitán del equipo de voleibol de la Universidad Panamericana (2021- Actual)")
+    st.markdown("###### Inversor y trader retail en mercados como el americano, la BMV y el CME, manejo de plataformas como GBM, NinjaTrader. (2019-Actual)")
+    st.markdown("###### Asesor de matemáticas de 6to semestre (Prepa UDG) (2020)")
+    st.markdown("###### Análisis corporativo “Schnitzer” (2023) y proyecto de inversión “Little Caesars” (2024)")
+
+
+
+   
